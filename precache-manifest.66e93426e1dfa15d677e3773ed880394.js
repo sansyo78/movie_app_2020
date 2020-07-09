5c9adcdf7fb8774d95cc09815eb98208055bb6e9
sansyo <sansyo78@gmail.com>
2020-07-09 15:05:38 +0900

    Updates

---
+++ precache-manifest.66e93426e1dfa15d677e3773ed880394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b9f395c0c4554584f135a5b25bb283",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c9011da72500012e6b01",
+    "url": "/movie_app_2020/static/css/main.35d17f43.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9011da72500012e6b01",
+    "url": "/movie_app_2020/static/js/main.0c8b384b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
