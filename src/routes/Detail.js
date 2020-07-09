dec08f6643b2ffc1d25917248a1c00a42a5118ec
sansyo <sansyo78@gmail.com>
2020-07-09 15:00:47 +0900

    commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../css/Detail.css";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <div className="detail__container">
+          <span>{location.state.title}</span>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Detail;
